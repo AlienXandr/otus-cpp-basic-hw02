109a3e02070662375623564637ac9d47f341f6b5
Alexandr Shostak <ashostak@t1.ru>
2024-04-09 17:52:14 +0100

    Закончена сборка приложения

---
+++ my_module/move_me_out.cpp
@@ -0,0 +1,9 @@
+//
+// Created by alexandr on 09.04.2024.
+//
+#include <iostream>
+#include "move_me_out.h"
+
+void todo_move_me_out() {
+    std::cout << "This function should be located in the separated source file" << std::endl;
+}
---
+++ my_module/move_me_out.h
@@ -0,0 +1,6 @@
+//
+// Created by alexandr on 09.04.2024.
+//
+#pragma once
+
+void todo_move_me_out();
